dbaf8dca71581856fa83a6f0fba6ac3e7eeff082
Leon Matthew O <79904658+leematt95@users.noreply.github.com>
2025-06-29 23:13:05 +0100

    change to the dockerfile and emoji input

--- app/main.py
+++ app/main.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Football Stats App is running!"
+    return "🏟️Football Stats App is running!"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
