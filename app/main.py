37ac24d7007fe0a766761d3b0d41b33d01c9c6b4
Leon Matthew O <79904658+leematt95@users.noreply.github.com>
2025-06-30 02:01:07 +0100

    debugger added and .env added

--- app/main.py
+++ app/main.py
@@ -1,9 +1,20 @@
 from flask import Flask
+from dotenv import load_dotenv
+import os
+
 app = Flask(__name__)
+load_dotenv()  # Load .env file variables
 
 @app.route("/")
 def home():
-    return "🏟️Football Stats App is running!"
+    return "🏟️ Football Stats App is running!"
+
+@app.route("/db-check")
+def check_env():
+    # Read values from .env
+    db_user = os.getenv("POSTGRES_USER")
+    db_pass = os.getenv("POSTGRES_PASSWORD")
+    return f"User: {db_user}, Pass: {db_pass}"
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    app.run(host="0.0.0.0", port=5000, debug=True)
